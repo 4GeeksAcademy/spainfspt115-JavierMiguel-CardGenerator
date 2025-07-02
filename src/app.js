ce485ceeca9f449fc8374e430d7537d2bd8c6c5a
Jmvdev <jvi96miguel@gmail.com>
2025-07-02 17:59:17 +0000

    Project finalized

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 import "bootstrap";
 import "./style.css";
 
+let cardHtml = "";
+let headCard = "";
+let contentCard = "";
+let bottomCard = "";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const card =
+{
+  suits: ["clubs", "hearts", "diamonds", "spades"],
+  suitsSymbols: ["♣", "♥", "♦", "♠"],
+  values: ["1", "2", "3", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]
 
-window.onload = function() {
+}
+
+//load function generate New Card
+window.onload = function CardGenerator() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  cardHtml = document.getElementById("pokerCard");
+  headCard = document.getElementById("pokerCardHead");
+  contentCard = document.getElementById("pokerCardContent");
+  bottomCard = document.getElementById("pokerCardBottom");
+
+  let randomValue = Math.floor(Math.random() * card.suits.length);
+
+  cardHtml.classList.add(card.suits[randomValue]);
+  headCard.innerText = card.suitsSymbols[randomValue];
+  contentCard.innerText = card.values[Math.floor(Math.random() * card.values.length)];
+  bottomCard.innerText = card.suitsSymbols[randomValue];
+
 };
+
+
